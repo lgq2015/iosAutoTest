a5c687ebf95fe15753109893c93a4945c322e17f
609223909@qq.com <wangwenyong@startimes.com.cn>
2021-04-01 17:53:54 +0800

    埋点

--- sniffer.py
+++ sniffer.py
@@ -1,42 +1,58 @@
-import logging
 from mitmproxy import http
 from mitmproxy import ctx
+import urllib.parse
 
-password_key = ["password", "pwd", "pass", "passwd", "mm", "passport", "auth", "key", "mima"]
-form_login_key = ["check", "login", "verify", "account", "logon", "signin", "denglu"]
+from mitmproxy.coretypes.multidict import MultiDictView
+
+getUrls = ["http://10.0.243.111:8080/i?logtype=event&events="]
+player_actions = {
+    "video_play": "视频开始播放时发送",
+    "video_pause": "视频暂停时报送",
+    "video_resume": "视频暂停后重新播放时报送",
+    "video_exit": "视频播放结束时发送",
+    "share_tap": "点击分享按钮时报送",
+    "video_replay": "点击视频重播按钮时发送"
+}
 
 
-class Sniffer:
-	def __init__(self):
-		self.num = 0
+class Counter:
+    def __init__(self):
+        self.num = 0
 
-	def request(self, flow: http.HTTPFlow):
-		self.num = self.num + 1
-		ctx.log.info("We've seen %d flows" % self.num)
-		request = flow.request
-		#print(request.headers)
-		ctx.log.info(str(request.headers))
-		url = request.pretty_url
-		form_url = str(request.urlencoded_form)
-		if request.method == 'GET':
-			if self.any_match(form_login_key, url) or self.any_match(password_key, form_url):
-				self.resolve(flow)
+    def any_match(self, list, str: str):
+        lower_str = str.lower()
+        for i in list:
+            if i in lower_str:
+                return True
+            return False
 
-	def any_match(self, list, str: str):
-		lower_str = str.lower()
-		for i in list:
-			if i in lower_str:
-				return True
-			return False
+    def request(self, flow: http.HTTPFlow):
+        request = flow.request
+        url = request.pretty_url
+        if request.method == 'GET' and self.any_match(getUrls, url):
+            self.resolve(request)
 
-	def resolve(self, flow: http.HTTPFlow):
-		request = flow.request
-		url = request.url
-		_from = str(flow.client_conn.ip_address)
-		url_form = str(request.urlencoded_form)
-		header = str(request.headers)
-		#logging.info("url: %s \nheader: %s \nform: %s \nip_from: %s", url, header, url_form, _from)
+    def resolve(self, request):
+        queryValue: MultiDictView = request.query
+        logtype = queryValue.get("logtype")
+        if logtype == "event":
+            events = queryValue.get("events")
+            if isinstance(events, str) and len(events) > 0:
+                action = eval(events)[0]["msg"]["action"]
+                print(action)
+                actions = player_actions.keys()
+                msg = player_actions.values()
+                if(self.any_match(actions, action)):
+                    print(msg+"正常")
+                else:
+                    print(msg+"不正常")
+
+        elif logtype == "pv":
+            print("pv")
+        else:
+            print("no logtype")
+
 
 addons = [
-    Sniffer()
+    Counter()
 ]
